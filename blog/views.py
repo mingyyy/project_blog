df8745404ad3f43a3a7ac41b16b05e853109f7b8
Mingyyy <j.yanming@gmail.com>
2019-04-10 09:08:43 +0800

    Add crispy form; UserRegisterForm

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts=[
-    {
-    'author': "Ming",
-    'title': "Blog Post 1",
-    'content': 'welcome to bali',
-    'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': "Martin",
-        'title': "Blog Post 2",
-        'content': 'exam time, are you ready',
-        'date_posted': 'August 27, 2019'
-
-    }]
-
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts,
+        'posts':Post.objects.all(),
         'title': "just"
     }
     return render(request, "blog/home.html", context)

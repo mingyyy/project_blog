b7e17b1efc2db85e8ee079fe650cf736a1c6378e
Mingyyy <j.yanming@gmail.com>
2019-04-17 20:44:06 +0800

    Fix default.jpg error for registration.

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -130,7 +130,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4" # default is 2
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "login"
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"{username} your account has been created! You are now ready to log in!")
-            return redirect('users:login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})

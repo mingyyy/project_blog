21416114c2a1781ff58f6717ca32824b93f63268
Ming <j.yanming@gmail.com>
2019-05-03 17:29:24 +0800

    Delete secret_blog.py

--- secret_blog.py
+++
@@ -1,7 +0,0 @@
-API_KEY = "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"
-SECRET_KEY = '7b)oa8f@bc_avhh_e^_24-3uxca9b8(a+uq^jz#a#)m@4jx!2w'
-S3_ACCESS_KEY_ID = "AKIAI2R5REMUSNJ5H3MQ"
-S3_SECRET_ACCESS_KEY = "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"
-S3_USER_NAME = "s3-ming"
-console_link = "https://122276501109.signin.aws.amazon.com/console"
-S3_STORAGE_BUCKET_NAME = 'ming-blog-assets'
